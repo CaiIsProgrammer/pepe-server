631c9bf1b05f33902946f9540c361bcdd9286afc
Cai Martin <cmartin.professional@yahoo.com>
2020-06-10 19:25:57 -0400

    update tag

--- packet.js
+++ packet.js
@@ -124,6 +124,7 @@ module.exports = packet = {
         c.broadcastEveryone(packet.build(["TAGGED", data.username]));
         break;
       case "TAGIMMUNE":
+        console.log("tag immune");
         data = PacketModels.msg.parse(dataPacket);
         await c.setTaggedImmune(data.username);
         c.broadcastRoom(packet.build(["TAGIMMUNE", data.username]));
